2fadf73353244865c2e2c0e7f88f097a02b31e3d
Wrietenea <124199058+Wrietenea@users.noreply.github.com>
2023-03-25 13:42:33 +0300

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -5,14 +5,34 @@ from pathlib import Path
 from sklearn.metrics import accuracy_score
 
 from train import load_data
-
+import numpy as np
+import pandas as pd
+from skimage.io import imread_collection
+from skimage.transform import resize
+from sklearn.linear_model import SGDClassifier
+import numpy as np
+import torch
+import torch.nn as nn
+from torchvision import datasets
+from torchvision import transforms
+from torch.utils.data.sampler import SubsetRandomSampler
 
 def main(repo_path):
-    test_csv_path = repo_path / "data/prepared/test.csv"
-    test_data, labels = load_data(test_csv_path)
+
     model = load(repo_path / "model/model.joblib")
-    predictions = model.predict(test_data)
-    accuracy = accuracy_score(labels, predictions)
+    
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            del images, labels, outputs
+    accuracy = correct/total
     metrics = {"accuracy": accuracy}
     accuracy_path = repo_path / "metrics/accuracy.json"
     accuracy_path.write_text(json.dumps(metrics))
@@ -21,3 +41,4 @@ def main(repo_path):
 if __name__ == "__main__":
     repo_path = Path(__file__).parent.parent
     main(repo_path)
+
