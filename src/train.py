73ca41397154a961923021d12db407e85264349c
Wrietenea <124199058+Wrietenea@users.noreply.github.com>
2023-03-26 13:20:59 +0300

    Update train.py

--- src/train.py
+++ src/train.py
@@ -9,8 +9,10 @@ from skimage.transform import resize
 import numpy as np
 import torch
 import torch.nn as nn
+import torch
+import torchvision
+import torchvision.transforms as transforms
 from torchvision import datasets
-from torchvision import transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 
 # Device configuration
@@ -67,115 +69,24 @@ class AlexNet(nn.Module):
         return out
 
 
-def load_images(data_frame, column_name):
-    filelist = data_frame[column_name].to_list()
-    image_list = imread_collection(filelist)
-    return image_list
+#loading the dataset
+transform = transforms.Compose(transforms.Resize(256),
+                                transforms.CenterCrop(224),
+                                transforms.ToTensor(),
+                                transforms.Normalize(mean=[0.485, 0.456, 0.406,
+                                 std=0.229, 0.224, 0.225)])
 
+#loading the dataset
+dataset = torchvision.datasets.ImageFolder(root=data_dir_train, transform=transform)
 
-def load_labels(data_frame, column_name):
-    label_list = data_frame[column_name].to_list()
-    return label_list
-
-
-def preprocess(image):
-    resized = resize(image, (100, 100, 3))
-    reshaped = resized.reshape((1, 30000))
-    return reshaped
-
-
-def load_data(data_path):
-    df = pd.read_csv(data_path)
-    labels = load_labels(data_frame=df, column_name="label")
-    raw_images = load_images(data_frame=df, column_name="filename")
-    processed_images = [preprocess(image) for image in raw_images]
-    data = np.concatenate(processed_images, axis=0)
-    return data, labels
-
-def get_train_valid_loader(data_dir_train,
-                           batch_size,
-                           augment,
-                           random_seed,
-                           valid_size=0.5,
-                           shuffle=True):
-    normalize = transforms.Normalize(
-        mean=[0.4914, 0.4822, 0.4465],
-        std=[0.2023, 0.1994, 0.2010],
-    )
-    
-    # define transforms
-    if augment:
-        train_transform = transforms.Compose([
-            transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            normalize,
-        ])
-    else:
-        train_transform = transforms.Compose([
-            transforms.Resize((227,227)),
-            transforms.ToTensor(),
-            normalize,
-        ])
-
-
-    # load the dataset
-    train_dataset = datasets.CIFAR10(
-        root=data_dir_train, train=True,
-        download=True, transform=train_transform,
-    )
-
-    num_train = len(train_dataset)
-    indices = list(range(num_train))
-    split = int(np.floor(valid_size * num_train))
-
-    if shuffle:
-        np.random.seed(random_seed)
-        np.random.shuffle(indices)
-
-    train_idx = indices[split:]
-    train_sampler = SubsetRandomSampler(train_idx)
-
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=batch_size, sampler=train_sampler)
-
-
-    return train_loader
-
-
-def get_test_loader(data_dir_test,
-                    batch_size,
-                    shuffle=True):
-    normalize = transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225],
-    )
-
-    # define transform
-    transform = transforms.Compose([
-        transforms.Resize((227,227)),
-        transforms.ToTensor(),
-        normalize,
-    ])
-
-    dataset = datasets.CIFAR10(
-        root=data_dir_test, train=False,
-        download=True, transform=transform,
-    )
-
-    data_loader = torch.utils.data.DataLoader(
-        dataset, batch_size=batch_size, shuffle=shuffle
-    )
-
-    return data_loader
-
+#loading the data into dataloader
+train_loader = torch.utils.data.DataLoader(dataset, batchsize=batch_size, shuffle=True, numworkers=1)
 
 def main(repo_path):
     data_path = repo_path / "data"
-    data_dir_train = data_path / "raw/train"
-    data_dir_test = data_path / "raw/test"
-    num_classes = 10
-    num_epochs = 5
+    data_dir_train = data_path / "hymenoptera_data/train"
+    num_classes = 2
+    num_epochs = 10
     batch_size = 64
     learning_rate = 0.005
     model = AlexNet(num_classes).to(device)
