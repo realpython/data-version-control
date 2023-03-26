217cf07ca9ccd69bf7e91c5a7d3a906633c39b77
Wrietenea <124199058+Wrietenea@users.noreply.github.com>
2023-03-26 12:43:16 +0300

    Update prepare.py

--- src/prepare.py
+++ src/prepare.py
@@ -1,105 +1,38 @@
 from pathlib import Path
 
 import pandas as pd
-import numpy as np
-import torch
-import torch.nn as nn
-from torchvision import datasets
-from torchvision import transforms
-from torch.utils.data.sampler import SubsetRandomSampler
 
+FOLDERS_TO_LABELS = {
+    "n03445777": "golf ball",
+    "n03888257": "parachute"
+    }
 
-#FOLDERS_TO_LABELS = {"n03445777": "golf ball", "n03888257": "parachute"}
+def get_files_and_labels(source_path):
+    images = []
+    labels = []
+    for image_path in source_path.rglob("*/*.JPEG"):
+        filename = image_path.absolute()
+        folder = image_path.parent.name
+        if folder in FOLDERS_TO_LABELS:
+            images.append(filename)
+            label = FOLDERS_TO_LABELS[folder]
+            labels.append(label)
+    return images, labels
 
-def get_train_valid_loader(data_dir_train,
-                           batch_size,
-                           augment,
-                           random_seed,
-                           valid_size=0.5,
-                           shuffle=True):
-    normalize = transforms.Normalize(
-        mean=[0.4914, 0.4822, 0.4465],
-        std=[0.2023, 0.1994, 0.2010],
-    )
-    
-    # define transforms
-    if augment:
-        train_transform = transforms.Compose([
-            transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            normalize,
-        ])
-    else:
-        train_transform = transforms.Compose([
-            transforms.Resize((227,227)),
-            transforms.ToTensor(),
-            normalize,
-        ])
-
-
-    # load the dataset
-    train_dataset = datasets.CIFAR10(
-        root=data_dir_train, train=True,
-        download=True, transform=train_transform,
-    )
-
-    num_train = len(train_dataset)
-    indices = list(range(num_train))
-    split = int(np.floor(valid_size * num_train))
-
-    if shuffle:
-        np.random.seed(random_seed)
-        np.random.shuffle(indices)
-
-    train_idx = indices[split:]
-    train_sampler = SubsetRandomSampler(train_idx)
-
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=batch_size, sampler=train_sampler)
-
-
-    return train_loader
-
-
-def get_test_loader(data_dir_test,
-                    batch_size,
-                    shuffle=True):
-    normalize = transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225],
-    )
-
-    # define transform
-    transform = transforms.Compose([
-        transforms.Resize((227,227)),
-        transforms.ToTensor(),
-        normalize,
-    ])
-
-    dataset = datasets.CIFAR10(
-        root=data_dir_test, train=False,
-        download=True, transform=transform,
-    )
-
-    data_loader = torch.utils.data.DataLoader(
-        dataset, batch_size=batch_size, shuffle=shuffle
-    )
-
-    return data_loader
-
+def save_as_csv(filenames, labels, destination):
+    data_dictionary = {"filename": filenames, "label": labels}
+    data_frame = pd.DataFrame(data_dictionary)
+    data_frame.to_csv(destination)
 
 def main(repo_path):
     data_path = repo_path / "data"
-    data_dir_train = data_path / "raw/train"
-    data_dir_test = data_path / "raw/test"
-    
-    train_loader = get_train_valid_loader(data_dir_train = './data/raw/train',                                      batch_size = 64,
-                        augment = False,                             		     random_seed = 1)
-
-    test_loader = get_test_loader(data_dir_test = './data/raw/test',
-                                batch_size = 64)
-
+    train_path = data_path / "raw/train"
+    test_path = data_path / "raw/val"
+    train_files, train_labels = get_files_and_labels(train_path)
+    test_files, test_labels = get_files_and_labels(test_path)
+    prepared = data_path / "prepared"
+    save_as_csv(train_files, train_labels, prepared / "train.csv")
+    save_as_csv(test_files, test_labels, prepared / "test.csv")
 
 if __name__ == "__main__":
     repo_path = Path(__file__).parent.parent
