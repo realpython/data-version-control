3aec819134ba920f8f379516b4ff81b0865b02e0
Wrietenea <nastya.prohorova5136@mail.ru>
2023-03-27 11:23:52 +0300

    onnx 8'

--- src/evaluate.py
+++ src/evaluate.py
@@ -13,6 +13,8 @@ import torchvision
 from torchvision import datasets
 from torchvision import transforms
 from torch.utils.data.sampler import SubsetRandomSampler
+import onnx
+import onnxruntime as ort
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 device = 'cuda'
@@ -34,29 +36,25 @@ def main(repo_path):
 
     #loading the data into dataloader
     test_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
-    eval_time = []
-    eval_time_pure = []
-    with torch.no_grad():
-        correct = 0
-        total = 0
-        for images, labels in test_loader:
-            val_start = time.time()
-            images = images.to(device)
-            labels = labels.to(device)
-            val_start_pure = time.time()
-            outputs = model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            eval_time_pure.append(time.time()-val_start_pure)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-            eval_time.append(time.time()-val_start)
-            del images, labels, outputs
-    accuracy = correct/total
-    eval_time_avg = np.mean(eval_time)
-    eval_time_pure_avg = np.mean(eval_time_pure)
-    metrics = {"accuracy": accuracy, "eval_time_avg": eval_time_avg, "eval_time_pure_avg": eval_time_pure_avg}
-    with open('metrics_eval.json', 'w') as f:
-        json.dump(metrics, f, indent=3)
+    
+    channels = 3
+    height = 224
+    width = 224
+    sample_input = torch.cuda.FloatTensor(batch_size, channels, height, width)
+
+    onnx_model_path = repo_path / "model"
+    torch.onnx.export(model,sample_input,onnx_model_path,opset_version=12,input_names=['input'],output_names=['output'])
+    # Load the ONNX model
+    onnx_path = 'C:/Users/nasty/data-version-control/model/model.onnx'
+    model = onnx.load(onnx_path)
+    ort_session = ort.InferenceSession('model.onnx')
+# Check that the IR is well formed
+    onnx.checker.check_model(model)
+    outputs = ort_session.run(
+    None,
+    {'input': np.random.randn(batch_size, channels, height, width).astype(np.float32)}
+)
+
 
 
 if __name__ == "__main__":
